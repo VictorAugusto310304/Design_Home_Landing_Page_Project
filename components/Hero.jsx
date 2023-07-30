4d2fba8c3a01cfde673409081b75ad675dcf3441
Victor Augusto da Silva Santos <victorass130@gmail.com>
2023-07-30 20:00:35 -0300

    Alterações 30/07/2023 #3 (Fazendo alterações na Home e adicionando rede sociais)

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import Button from './Button'
+import Link from 'next/link'
+import {BsFacebook, BsTwitter, BsYoutube, BsLinkedin} from 'react-icons/bs'
 
-const Hero = () =>{
+export default function Hero() {
     return(
-        <div className='bg-[url("/living.jpg")] h-screen object-scale-down'></div>
+        <div className=' bg-[url("/living.jpg")] h-screen object-scale-down bg-cover bg-no-repeat bg-center
+        bg-fixed flex items-center relative '>
+            <div className=' absolute inset-0 bg-black/60 '></div>
+            <div className=' container mx-auto px-4 z-10 '>
+                <div className=' max-w-[450px] text-white flex flex-col gap-[40px] '>
+                    <div>
+                        <h1 className=' text-4xl md:text-5xl font-semibold tracking-wide '>Design your Dream</h1>
+                    </div>
+                    <div>
+                        <h1 className=' text-4xl md:text-5xl font-semibold tracking-wide '><span className='text-green-600'>Home</span> Very Easily</h1>
+                    </div>
+                    <div className=' bg-green-600 h-[2px] w-[300px] '></div>
+                    <div>
+                        <p className=' text-lg md:text-xl text-gray-300 tracking-widest '>Simple Furniture and High-end Quality.</p>
+                    </div>
+                    <div className=' bg-green-600 px-4 py-2 w-[180px] text-center rounded-lg inline-block font-semibold border border-green-600 hover:bg-transparent hover:text-green-600 transition duration-300 '>
+                        <Button link="#" text="Explore Products"/>
+                    </div>
+                </div>
+            <div className=' hidden absolute right-20 top-32 z-10 flex flex-col gap-5 items-center '>
+                <div className=' bg-green-600 w-[1.5px] h-[250px] '></div>
+                <Link  href="https://www.facebook.com" target="_blank"><BsFacebook size={25} className=' text-green-600 hover:-translate-x-1.5 duration-300 '/></Link>
+                <Link  href="https://www.twitter.com" target="_blank"><BsTwitter size={25} className=' text-green-600 hover:-translate-x-1.5 duration-300 '/></Link>
+                <Link  href="https://www.youtube.com" target="_blank"><BsYoutube size={25} className=' text-green-600 hover:-translate-x-1.5 duration-300 '/></Link>
+                <Link  href="https://www.linkedin.com" target="_blank"><BsLinkedin size={25} className=' text-green-600 hover:-translate-x-1.5 duration-300 '/></Link>
+            </div>
+            </div>
+        </div>
     )
 }
-
-export default Hero
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,42 +4,40 @@ import {FaHome} from 'react-icons/fa';
 import {FiMenu} from 'react-icons/fi';
 import {AiOutlineClose} from 'react-icons/ai'
 
-const Navbar = () =>{
+export default function Navbar() {
 
     const [toggle, setToggle] = useState(false);
     
     return (
-        <div className='fixed bg-white top-0 w-[100%] z-20'>
-            <div className='container nx-auto flex justify-between items-center px-4 py-4'>
-                <div className='flex gap-1 items-center text-xl md:text-2xl font-bold'>
-                    <span className='italic'>Art</span>
+        <div className=' fixed bg-white top-0 w-[100%] z-20 '>
+            <div className=' container nx-auto flex justify-between items-center px-4 py-4 '>
+                <div className=' flex gap-1 items-center text-4xl md:text-3xl font-bold '>
+                    <span className=' italic '>Art</span>
                     <FaHome/>
-                    <span className='italic text-green-700'>Design</span>
+                    <span className=' italic text-green-700 '>Design</span>
                 </div>
-                <div className='hidden md:flex gap-6 tracking-wider text-gray-600 font-bold'>
-                    <Link href="/" className='hover:text-green-700'>Home</Link>
-                    <Link href="/" className='hover:text-green-700'>Products</Link>
-                    <Link href="/" className='hover:text-green-700'>Categories</Link>
-                    <Link href="/" className='hover:text-green-700'>Services</Link>
+                <div className=' hidden md:flex gap-9 tracking-wider text-gray-600 font-bold text-base '>
+                    <Link href="/" className=' hover:text-green-700 '>Home</Link>
+                    <Link href="/" className=' hover:text-green-700 '>Products</Link>
+                    <Link href="/" className=' hover:text-green-700 '>Categories</Link>
+                    <Link href="/" className=' hover:text-green-700 '>Services</Link>
                 </div>
                 <div>
-                    <button className='hidden md:block border border-green-700 px-4 py-1 rounded-md text-green-700 hover:bg-green-700 hover:text-white'>Contact</button>
+                    <button className=' hidden md:block border border-green-700 px-4 py-1 rounded-md text-green-700 hover:bg-green-700 hover:text-white '>Contact</button>
                 </div>
                 {toggle ? (
-                    <AiOutlineClose onClick={()=>setToggle(!toggle)} size={30} className='md:hidden block'/>
+                    <AiOutlineClose onClick={()=>setToggle(!toggle)} size={40} className=' md:hidden block '/>
                 ):(
-                    <FiMenu onClick={()=>setToggle(!toggle)} size={30} className='md:hidden block'/>
+                    <FiMenu onClick={()=>setToggle(!toggle)} size={40} className=' md:hidden block '/>
                 )}
             </div>
             {/* Menu Responsivo*/}
-            <div className={` duration-300 md:hidden flex flex-col w-[70%] h-screen fixed bg-black/70 text-white top-[60px] ${toggle ? `left-[0]` : `left-[-100%]`}`}>
-                    <Link href="/" className='hover:text-green-700 p-5'>Home</Link>
-                    <Link href="/" className='hover:text-green-700 p-5'>Products</Link>
-                    <Link href="/" className='hover:text-green-700 p-5'>Categories</Link>
-                    <Link href="/" className='hover:text-green-700 p-5'>Services</Link>
+            <div className={` duration-300 md:hidden flex flex-col w-[70%] h-screen fixed bg-black/70 text-white top-[72px] ${toggle ? `left-[0]` : `left-[-100%]`} `}>
+                    <Link href="/" className=' hover:text-green-700 p-5 '>Home</Link>
+                    <Link href="/" className=' hover:text-green-700 p-5 '>Products</Link>
+                    <Link href="/" className=' hover:text-green-700 p-5 '>Categories</Link>
+                    <Link href="/" className=' hover:text-green-700 p-5 '>Services</Link>
                 </div>
         </div>
     );
 }
-
-export default Navbar
